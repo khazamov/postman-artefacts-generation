43f3dc146e1f0e0255f833913706460211524682
o.khazamov <omar.khazamov@github.com>
2022-11-30 17:20:04 +0100

    Add conversion script and modify the workflow

---
+++ convert_to_postman_spec.sh
@@ -0,0 +1,6 @@
+rm -f api-postman-specifications/*
+
+for file in api-specifications/*
+do
+  openapi2postmanv2 -s "$file" -o api-postman-specifications/"$(basename -- $file)" -p -O folderStrategy=Tags,requestParametersResolution=Schema,responseParametersResolution=Schema,optimizeConversion=false,stackLimit=50
+done
